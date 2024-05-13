9f868159bcca8f99a5fbbe2e3b08e5328c0e6d02
vinay-k-sharma <vinay.sharma@bacancy.com>
2024-05-13 15:01:08 +0530

    [FEAT]:Added optimized cached connection to mongodb database

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -4,4 +4,14 @@ let cached = mongoose || {conn:null, promise:null}
 
 export const connectToDatabase = async () => {
     if(cached.conn) return cached.conn
+
+    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing')
+
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI,{
+        dbName:'Bac-Club',
+        bufferCommands:false
+    })
+    cached.conn = await cached.promise
+
+    return cached.conn
 }
