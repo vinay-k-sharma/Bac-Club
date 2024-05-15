1aa75e183d0d9d38ce702af147931c7e9b2b014e
vinay-k-sharma <vinay.sharma@bacancy.com>
2024-05-15 10:58:25 +0530

    [Refactor]: Resolved type issues in route file of webhook

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -61,8 +61,8 @@ export async function POST(req: Request) {
         clerkId: id,
         email : email_addresses[0].email_address,
         username:username!,
-        firstName:first_name,
-        lastName:last_name,
+        firstName:first_name || "",
+        lastName:last_name || "",
         photo:image_url,
     }
 
@@ -81,8 +81,8 @@ export async function POST(req: Request) {
   if (eventType === 'user.updated') {
     const {id,image_url,first_name,last_name,username} = evt.data
     const user = {
-        firstName:first_name,
-        lastName:last_name,
+        firstName:first_name || "",
+        lastName:last_name || "",
         username:username!,
         photo:image_url,
     }
