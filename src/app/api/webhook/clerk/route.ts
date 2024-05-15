ad5cb446d2aa589b0264e2bfded26ec4fa302f2d
vinay-k-sharma <vinay.sharma@bacancy.com>
2024-05-15 12:40:30 +0530

    [FEAT]: Added delete user functionality

--- src/app/api/webhook/clerk/route.ts
+++ src/app/api/webhook/clerk/route.ts
@@ -1,7 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent, clerkClient } from '@clerk/nextjs/server'
-import { createUser, updateUser } from '@/lib/actions/user.actions'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
@@ -89,10 +89,10 @@ export async function POST(req: Request) {
     const updatedUser = await updateUser(id,user)
     return NextResponse.json({message:'OK',user:updatedUser})
   }
-  // if(eventType==='user.deleted'){
-  //   const {id} = evt.data
-  //   const deletedUser = await deleteUser(id!)
-  //   return NextResponse.json({message:'OK',user:deletedUser})
-  // }
+  if(eventType==='user.deleted'){
+    const {id} = evt.data
+    const deletedUser = await deleteUser(id!)
+    return NextResponse.json({message:'OK',user:deletedUser})
+  }
   return new Response('', { status: 200 })
 }
--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -46,35 +46,35 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
   }
 }
 
-// export async function deleteUser(clerkId: string) {
-//   try {
-//     await connectToDatabase()
+export async function deleteUser(clerkId: string) {
+  try {
+    await connectToDatabase()
 
-//     // Find user to delete
-//     const userToDelete = await User.findOne({ clerkId })
+    // Find user to delete
+    const userToDelete = await User.findOne({ clerkId })
 
-//     if (!userToDelete) {
-//       throw new Error('User not found')
-//     }
+    if (!userToDelete) {
+      throw new Error('User not found')
+    }
 
-//     // Unlink relationships
-//     await Promise.all([
-//       // Update the 'events' collection to remove references to the user
-//       Event.updateMany(
-//         { _id: { $in: userToDelete.events } },
-//         { $pull: { organizer: userToDelete._id } }
-//       ),
+    // Unlink relationships
+    // await Promise.all([
+    //   // Update the 'events' collection to remove references to the user
+    //   Event.updateMany(
+    //     { _id: { $in: userToDelete.events } },
+    //     { $pull: { organizer: userToDelete._id } }
+    //   ),
 
-//       // Update the 'orders' collection to remove references to the user
-//       Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
-//     ])
+    //   // Update the 'orders' collection to remove references to the user
+    //   Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
+    // ])
 
-//     // Delete user
-//     const deletedUser = await User.findByIdAndDelete(userToDelete._id)
-//     revalidatePath('/')
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id)
+    revalidatePath('/')
 
-//     return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
+  } catch (error) {
+    console.log(error)
+  }
+}
