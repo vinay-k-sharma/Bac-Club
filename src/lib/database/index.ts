41efdff0440f1a22475d8352b09e787ac3d07224
vinay-k-sharma <vinay.sharma@bacancy.com>
2024-05-15 11:13:18 +0530

    [Refactored]: Resolved Type issues in mngodb connection file(index.ts)

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -1,17 +1,20 @@
-import mongoose from 'mongoose'
-const MONGODB_URI = process.env.MONGODB_URI
-let cached = mongoose || {conn:null, promise:null}
+import mongoose from 'mongoose';
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-    if(cached.conn) return cached.conn
+  if (cached.conn) return cached.conn;
 
-    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing')
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI,{
-        dbName:'Bac-Club',
-        bufferCommands:false
-    })
-    cached.conn = await cached.promise
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
 
-    return cached.conn
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
